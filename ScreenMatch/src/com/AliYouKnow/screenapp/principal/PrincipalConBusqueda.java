c4fa001ce2e10d26e4ee899f165b9d2afcd5647b
Ali Israel <asteteali@gmail.com>
2025-07-10 16:28:24 -0500

    Haciendo las peticiones

--- ScreenMatch/src/com/AliYouKnow/screenapp/principal/PrincipalConBusqueda.java
+++ ScreenMatch/src/com/AliYouKnow/screenapp/principal/PrincipalConBusqueda.java
@@ -1,19 +1,36 @@
 package com.AliYouKnow.screenapp.principal;
 
+import java.io.IOException; //errores exception
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
-    public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+    //System.out.println("Escriba el nombre de la pelicula: ");
+
+    var busqueda = teclado.nextLine();
+
+    String direccion = "https://www.omdbapi.com/?i=" + busqueda + "&apikey=fb5eae08";
+
+    //este codigo no deberia estar en el main
+    public static void main(String[] args) throws IOException, InterruptedException {
         //Hacer una request/peticion a un servidor usando verbos http
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://"))
+                .uri(URI.create(direccion))
                 .build();
+// HttpRequest req = new HttpRequest(); <- es abstract como si fuera una "interfaz"
+//por eso se usa builder
+/*Recibir los datos despues de pedir*/
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
 
-        
+        System.out.println(response.body());
 
 
     }
